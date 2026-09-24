ac522c6358e3a407289a7ad626349e94fe162e68
Sadeq <sadeq.al-rumaih@ibm.com>
2026-09-24 15:26:29 +0300

    Add Google client and sheet IDs

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Neither value is a secret. The OAuth client ID is public by design,
 // and the sheet is protected by its Google sharing settings.
 window.LEDGER_CONFIG = {
-  CLIENT_ID: "YOUR_OAUTH_CLIENT_ID.apps.googleusercontent.com",
-  SHEET_ID: "YOUR_GOOGLE_SHEET_ID"
+  CLIENT_ID: "776960503437-i41g3oqb8tg2ovjhmrienddhs373hg6f.apps.googleusercontent.com",
+  SHEET_ID: "1_xLGYYLTATi2ufOb7kBUyc3qPH-r8r-k0EXF5q-2PKE"
 };
